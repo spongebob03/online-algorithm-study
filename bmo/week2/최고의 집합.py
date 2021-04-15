299243292df7a6fa3a58bcfb809f70c6ec05aaa0
spongebob03 <bmofinnjake08@gmail.com>
2021-04-15 23:07:39 +0900

    최고의 집합 (효율성 통과)

--- bmo/week2/최고의 집합.py
+++ bmo/week2/최고의 집합.py
@@ -1,21 +1,9 @@
-import itertools
-
 def solution(n, s):
-    answer = []
-    numbers = [_ for _ in range(1, s)]
-    combis = set()
+    if n > s: return [-1]
 
-    for combi in itertools.combinations(numbers, n):
-        if sum(combi) == s:
-            combis.add((combi[0] * combi[1], combi))
-    if s % 2 == 0: combis.add(((s//2)**2, (s//2, s//2)))
-    
-    if not combis: return [-1]
-    for number in sorted(list(combis), reverse= True)[0][1]:
-        answer.append(number)
-    
-    return answer
+    answer = [s//n for _ in range(n)]
 
-if __name__ == '__main__':
-    result = solution(2, 8)
-    print(result)
+    for idx in range(s % n):
+        answer[idx] += 1
+
+    return sorted(answer)
