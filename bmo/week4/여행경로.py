bce96b0d401338a0981cfef2812f147cedf4b876
spongebob03 <bmofinnjake08@gmail.com>
2021-05-01 21:24:50 +0900

    여행경로

---
+++ bmo/week4/여행경로.py
@@ -0,0 +1,21 @@
+def solution(tickets):
+    answer = []
+    flight = dict()
+
+    for source, destination in tickets:
+        flight[source] = flight.get(source, []) + [destination]
+    for source in flight:
+        flight[source].sort(reverse=True)
+    
+    stack = ["ICN"]
+
+    while stack:
+        if stack[-1] not in flight.keys() or not flight[stack[-1]]:
+            answer.append(stack.pop())
+        else:
+            stack.append(flight[stack[-1]].pop())
+    
+    return answer[::-1]
+
+if __name__ == '__main__':
+    print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]		))
