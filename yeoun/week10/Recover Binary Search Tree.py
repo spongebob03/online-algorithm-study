fcc4dab174298b522016e50cb14cd73649a3915a
dudns <dudns6984@naver.com>
2021-06-27 16:14:55 +0900

    주말 세션 이후 수정

--- yeoun/week10/Recover Binary Search Tree.py
+++ yeoun/week10/Recover Binary Search Tree.py
@@ -1,5 +1,3 @@
-# BST가 되긴 하는데, 한 번만 swap한 것과 결과가 다를 수 있어 틀림 
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,33 +6,29 @@
 #         self.right = right
 
 class Solution:
-    # all nodes in left subtree should be smaller than root value  
-    # if not, swap 
-    def left_dfs(self, curNode, root):  
-        if curNode:
-            if curNode.val > root.val:
-                curNode.val, root.val = root.val, curNode.val
-            
-            children = [curNode.left, curNode.right]
-            for child in children:
-                if child:
-                    self.left_dfs(child, root)
-    # all nodes in right subtree should be bigger than root value  
-    # if not, swap 
-    def right_dfs(self, curNode, root):        
-        if curNode:
-            if curNode.val < root.val:
-                curNode.val, root.val = root.val, curNode.val
-            
-            children = [curNode.left, curNode.right]
-            for child in children:
-                if child:
-                    self.right_dfs(child, root)
-
-    
     def recoverTree(self, root: TreeNode) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        self.left_dfs(root.left, root)
-        self.right_dfs(root.right, root)
+        result, swap = self.inorder(root, result=[], swap=[])
+        # swap의 첫 번째 값과 마지막 값을 서로 교환 
+        swap[0].val, swap[-1].val = swap[-1].val, swap[0].val
+
+    
+    # inorder traversal
+    def inorder(self, root, result, swap):
+        node = root
+        if node:
+            # 왼쪽 자식
+            if node.left:
+                result, swap = self.inorder(node.left, result, swap)
+            # 대소관계 맞지 않으면 swap에 저장 
+            if result and result[-1].val >= node.val:
+                swap += [result[-1], node]
+            # 루트
+            result.append(node)
+            # 오른쪽 자식 
+            if node.right:
+                result, swap = self.inorder(node.right, result, swap)
+        return result, swap
+        
