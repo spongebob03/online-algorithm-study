468f6886462d5951f19bb8108419548a1b4918ff
spongebob03 <bmofinnjake08@gmail.com>
2021-08-16 12:17:37 +0900

    뒤집기 수정(통과)

--- bmo/week16_greedy/뒤집기.py
+++ bmo/week16_greedy/뒤집기.py
@@ -3,13 +3,11 @@ arr = '1111011'
 def solution(arr):
     group = [0, 0]
 
-    for i in range(1, len(arr)):
-        if arr[i-1] != arr[i]:
-            group[int(arr[i-1])] += 1
+    group[int(arr[0])] += 1
+    for i in range(len(arr)-1):
+        if arr[i] != arr[i+1]:
+            group[int(arr[i+1])] += 1
             
-            if i == len(arr) - 1:
-                group[int(arr[i])] += 1
-
     return min(group)
 
 print(solution(arr))
